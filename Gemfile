d86c39ea72c92d5b283b390ae5f326754877b19e
FireEnginner <changemymind6@gmail.com>
2021-08-28 21:39:51 +0900

    fix

--- Gemfile
+++ Gemfile
@@ -25,10 +25,14 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+
 group :development, :test do
+  gem 'faker'
+  gem 'gimei'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+200.times do |n|
+  sample_name = Gimei.kanji
+  # sample_kana = Gimei.katakana
+  # sample_email = Faker::Internet.email
+  # sample_tel = Faker::Number.number(digits: 11)
+  # start_time = Faker::Date.forward(50)
+# start_time = Faker::Date.between(from: '2021-01-01', to: '2021-04-30') #期間を限定することもできます。
+  Demo.create!(
+    name: sample_name,
+    age:  n,
+  )
+end
